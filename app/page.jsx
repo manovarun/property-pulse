7ea6296858b7c500bae8bcb0f2242251d389a509
manovarun <samvarun@gmail.com>
2024-03-22 13:31:21 +0530

    Home page random property listings

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,14 @@ import Hero from '@/components/Hero';
 import Link from 'next/link';
 import InfoBoxes from '@/components/InfoBoxes';
 import Footer from '@/components/Footer';
+import HomeProperties from '@/components/HomeProperties';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
-      <Footer />
+      <HomeProperties />
     </>
   );
 };
