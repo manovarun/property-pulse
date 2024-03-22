4cb378b7b6dd0f312d2bcc954095e9b36486fc46
manovarun <samvarun@gmail.com>
2024-03-22 13:49:11 +0530

    NotFound page and Loader update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,15 +10,11 @@ const Footer = () => {
     <footer className="bg-gray-200 py-4 mt-24">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
         <div className="mb-4 md:mb-0">
-          <Image src={logo} alt="Logo" className="h-8 w-auto" />
+          <Link href="/">
+            <Image src={logo} alt="Logo" className="h-8 w-auto" />
+          </Link>
         </div>
-        <div className="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0">
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="/properties">Properties</Link>
-            </li>
-          </ul>
-        </div>
+
         <div>
           <p className="text-sm text-gray-500 mt-2 md:mt-0">
             &copy; {currentYear} PropertyPulse. All rights reserved.
