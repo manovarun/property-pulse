7316677f31a5f4c788c63999401ea524d17c8ac6
manovarun <samvarun@gmail.com>
2024-03-29 07:46:44 +0530

    Add Property POST API Route

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -16,3 +16,53 @@ export const GET = async (request) => {
     return new Response('Error on finding properties', { status: 500 });
   }
 };
+
+//POST /api/properties
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const formData = await request.formData();
+
+    const amenities = formData.getAll('amenities');
+    const images = formData
+      .getAll('images')
+      .filter((image) => image.name != '');
+
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('weekly'),
+        monthly: formData.get('monthly'),
+        nightly: formData.get('nightly'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      images,
+    };
+
+    const property = await Property.create({ ...propertyData });
+
+    return new Response(JSON.stringify(property), {
+      status: 201,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Error on creating property', { status: 500 });
+  }
+};
--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -78,7 +78,11 @@ const PropertyAddForm = () => {
 
   return (
     mounted && (
-      <form>
+      <form
+        action="/api/properties"
+        method="POST"
+        encType="multipart/form-data"
+      >
         <h2 className="text-3xl text-center font-semibold mb-6">
           Add Property
         </h2>
@@ -543,6 +547,7 @@ const PropertyAddForm = () => {
             accept="image/*"
             multiple
             onChange={handleImageChange}
+            required
           />
         </div>
 
